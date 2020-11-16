9a002f2da3093c419461088a297a90009a73ff66
Faiz <faiz.anwar4749@gmail.com>
2020-11-16 19:56:46 +0530

    [Faiz] Add: Query to create address book table

---
+++ AddressBookDB/AddressBookDB/UC2-CreateAddressBookTable.sql
@@ -0,0 +1,12 @@
+USE AddressBookDB;
+CREATE TABLE address_book
+(
+first_name varchar(25) NOT NULL,
+last_name varchar(25) NOT NULL,
+address varchar(150) NOT NULL,
+city varchar(25) NOT NULL,
+state varchar(25) NOT NULL,
+zip int NOT NULL,
+phone_number int NOT NULL,
+email varchar(100) NOT NULL
+);
