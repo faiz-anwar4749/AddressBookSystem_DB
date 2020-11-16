2642804cff60e2a35be7575d13b9c2de93f4ac9d
Faiz <faiz.anwar4749@gmail.com>
2020-11-16 21:40:36 +0530

    [Faiz] Add: Query to insert values in address book table.

---
+++ AddressBookDB/AddressBookDB/UC3-InsertContactsInTable.sql
@@ -0,0 +1,4 @@
+USE AddressBookDB;
+INSERT INTO address_book VALUES
+('Faiz','Anwar','Jadavpur','Kolkata','West Bengal', 700075, 942143006,'faiz.anwar@gmail.com'),
+('Chiks','Raj','Chingrighata','Kolkata','West Bengal', 700073, 973314851,'raj.chiks@gmail.com');
